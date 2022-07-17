fbeee5f1f4e6098d0f62e7564276ee15844c4f74
Viraj Patidar <vrj381@gmail.com>
2022-07-17 11:18:26 +0530

    current status component fixed

--- src/components/CurrentStatus.jsx
+++ src/components/CurrentStatus.jsx
@@ -4,42 +4,56 @@ import { getCurrentStatus } from "../services/monitorServices";
 import Box from "@mui/material/Box";
 
 const CurrentStatus = ({ currentStatusData }) => {
-  const [data, setData] = useState(null);
+    const [data, setData] = useState(null);
 
-  useEffect(() => {
-    getCurrentStatus(currentStatusData.id).then((d) => setData(d));
-  }, []);
-  console.log({ data });
+    useEffect(() => {
+        getCurrentStatus(currentStatusData.id).then((d) => setData(d.data));
+    }, []);
+    console.log({ data });
 
-  return (
-    <Box sx={{ ml: 6 }}>
-      {data ? (
-        <>
-          <div>
-            <b>Response Status Code</b>: {data.status}
-          </div>
-          <div>
-            <b>Response Status Text</b>: {data.statusText}
-          </div>
-          <div>
-            <b>Content Length</b>: {data.headers["content-length"]}
-          </div>
-          <div>
-            <b>Content Type</b>: {data.headers["content-type"]}
-          </div>
-          <div>
-            <b>
-              {data.status < 400
-                ? "Congratulations! The URL is Live!"
-                : "OOPS! The URL met an error!"}
-            </b>
-          </div>
-        </>
-      ) : (
-        "Processing Request..."
-      )}
-    </Box>
-  );
+
+    return (
+        <Box sx={{ ml: 6 }}>
+            {data ? (
+                <>
+                    <div>
+                        <b>Response Time</b>: {data.time_taken}
+                    </div>
+                    <div>
+                        <b>Response Status Code</b>: {data.status_code}
+                    </div>
+                    <div>
+                        <b>Response Status Text</b>: {data.status_text}
+                    </div>
+                    <div>
+                        <b>Content Length</b>: {data.content_length}
+                    </div>
+                    <div>
+                        <b>Content Type</b>: {data.content_type}
+                    </div>
+                    {["GET"].includes(data.httpMethod) && data.isAPI && (
+                        <>
+                            <div>
+                                <b>JSON Response</b>:
+                            </div>
+                            <div>
+                                <pre>{JSON.stringify(data.jsonBody, null, 2)}</pre>
+                            </div>
+                        </>
+                    )}
+                    <div>
+                        <b>
+                            {data.status_code < 400
+                                ? "Congratulations! The URL is Live!"
+                                : "OOPS! The URL met an error!"}
+                        </b>
+                    </div>
+                </>
+            ) : (
+                "Processing Request..."
+            )}
+        </Box>
+    );
 };
 
 export default CurrentStatus;
